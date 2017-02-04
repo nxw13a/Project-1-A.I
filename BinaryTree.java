fe132300c78b2ff571b295f0e682ca1b85f685fe
nxw13a <nxw13a@acu.edu>
2017-02-03 21:02:38 -0600

    Add Binary Tree and Node

--- BinaryTree.java
+++ BinaryTree.java
@@ -3,208 +3,70 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.*;
 import java.util.*;
 
 
 
 public class BinaryTree {
 
-	Node root;
-
-	public void addNode(int key, boolean name, int depth) {
-
-		// Create a new Node and initialize it
-
-		Node newNode = new Node(key, name, depth);
-
-		// If there is no root this becomes root
-
-		if (root == null) {
-
-			root = newNode;
-
-		} else {
-
-			// Set root as the Node we will start
-			// with as we traverse the tree
-
-			Node focusNode = root;
-
-			// Future parent for our new Node
-
-			Node parent;
-
-			while (true) {
-
-				// root is the top parent so we start
-				// there
-
-				parent = focusNode;
-
-				// Check if the new node should go on
-				// the left side of the parent node
-
-				if (key < focusNode.key) {
-
-					// Switch focus to the left child
-
-					focusNode = focusNode.leftChild;
-
-					// If the left child has no children
-
-					if (focusNode == null) {
-
-						// then place the new node on the left of it
-
-						parent.leftChild = newNode;
-						return; // All Done
-
-					}
-
-				} else { // If we get here put the node on the right
-
-					focusNode = focusNode.rightChild;
-
-					// If the right child has no children
-
-					if (focusNode == null) {
-
-						// then place the new node on the right of it
-
-						parent.rightChild = newNode;
-						return; // All Done
-
-					}
-
-				}
-
-			}
-		}
-
-	}
-
-	// All nodes are visited in ascending order
-	// Recursion is used to go to one node and
-	// then go to its child nodes and so forth
-
-	public void inOrderTraverseTree(Node focusNode) {
-
-		if (focusNode != null) {
-
-			// Traverse the left node
-
-			inOrderTraverseTree(focusNode.leftChild);
-
-			// Visit the currently focused on node
-
-			System.out.println(focusNode);
-
-			// Traverse the right node
-
-			inOrderTraverseTree(focusNode.rightChild);
-
-		}
-
-	}
-
-	public void preorderTraverseTree(Node focusNode) {
-
-		if (focusNode != null) {
-
-			System.out.println(focusNode);
-
-			preorderTraverseTree(focusNode.leftChild);
-			preorderTraverseTree(focusNode.rightChild);
-
-		}
-
-	}
-
-	public void postOrderTraverseTree(Node focusNode) {
-
-		if (focusNode != null) {
-
-			postOrderTraverseTree(focusNode.leftChild);
-			postOrderTraverseTree(focusNode.rightChild);
-
-			System.out.println(focusNode);
-
-		}
-
-	}
-
-	public Node findNode(int key) {
-
-		// Start at the top of the tree
-
-		Node focusNode = root;
-
-		// While we haven't found the Node
-		// keep looking
-
-		while (focusNode.key != key) {
-
-			// If we should search to the left
-
-			if (key < focusNode.key) {
-
-				// Shift the focus Node to the left child
-
-				focusNode = focusNode.leftChild;
-
-			} else {
-
-				// Shift the focus Node to the right child
-
-				focusNode = focusNode.rightChild;
-
-			}
-
-			// The node wasn't found
-
-			if (focusNode == null)
-				return null;
-
-		}
-
-		return focusNode;
-
-	}
+public static int maximum(int x)
+{
+    if(x == 1)
+        return 2;
+    return (int)Math.pow(2,x) + maximum(x-1);
+}
+// (rightchild - 2) / 2 = parent
+// (leftchild - 1) / 2 = parent
 
 public static void main(String[] args) {
 
-		BinaryTree theTree = new BinaryTree();
+        int num_item = 2;
+        int limit = 5;
+        String[] name = {"a","b"};
+        int[] cost = {2,3};
+        int[] value = {4,5};
+
+
 		System.out.println("Work on this Nattapat");
-		
+        Node a = new Node("","",-1);
+		List<Node> b = new ArrayList<Node>();
+        b.add(a);
+        System.out.println(b.get(0).item);
+
+        int y = 1;
+
+        for(int x = 0; x < num_item; x++)
+        {
+            while(y <= maximum(x+1))
+            {
+                String hold;
+                if(y % 2 == 0)
+                    hold = "T";
+                else
+                    hold = "F";
+
+                Node temp = new Node(name[x],hold,x);
+                b.add(temp);
+                y++;
+            }
+        }
+        for(int x = 1; x < b.size();x++)
+            System.out.println(b.get(x).item+b.get(x).choice+b.get(x).depth);
 }
 }
 
 class Node {
 
-	int key;
-	boolean name;
+	String item;
+	String choice;
 	int depth;
 
-	Node leftChild;
-	Node rightChild;
+	Node(String item, String choice, int depth) {
 
-	Node(int key, boolean name, int depth) {
-
-		this.key = key;
-		this.name = name;
+		this.item = item;
+		this.choice = choice;
 		this.depth = depth;
 
 	}
-
-	public String toString() {
-
-		return name + " has the key " + key;
-
-		/*
-		 * return name + " has the key " + key + "\nLeft Child: " + leftChild +
-		 * "\nRight Child: " + rightChild + "\n";
-		 */
-
-	}
-
 }
